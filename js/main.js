f81f9d3de217e2048abe2a0974d455e4b327fd2e
mdarafat2201 <maccenarafat@gmail.com>
2024-01-07 12:01:58 +0000

    guessign player one number

--- js/main.js
+++ js/main.js
@@ -4,24 +4,47 @@ const Player1Input = document.querySelector(".Player1Input");
 const gameStart = document.querySelector(".gameStart");
 
 //player tow markup refercence
-const playertowbox = document.querySelector(".playertowbox");
-
+const Player2Input = document.querySelector(".Player2Input");
+const guess = document.querySelector(".guess");
+let playeronevalue = 0;
+let chance = 4;
 gameStart.addEventListener("click", function () {
   if (
     Player1Input.value != "" &&
     Player1Input.value >= 0 &&
     Player1Input.value <= 10
   ) {
+    playeronevalue = Player1Input.value;
     error.innerHTML = "good valid input";
     error.style.color = "green";
     // hide all information of player one
+    Player1.style.display = "none";
     error.style.display = "none";
     Player1Input.style.display = "none";
     gameStart.style.display = "none";
     // visible all markup in player tow
-    playertowbox.style.display = "block";
+    Player1.style.display = "block";
+    Player1.innerHTML = "player 2";
+    Player2Input.style.display = "block";
+    guess.style.display = "block";
   } else {
     error.innerHTML = "invalid Crcdential";
+    error.style.color = "red";
+  }
+});
+//guessign player one number
+guess.addEventListener("click", function () {
+  if (
+    Player2Input.value != "" &&
+    Player2Input.value >= 0 &&
+    Player2Input <= 10
+  ) {
+    chance--;
+    error.innerHTML = `your guess is  wrong and  you have chance ${chance}`;
+    error.style.color = "green";
+  } else {
+    error.style.display = "block";
+    error.innerHTML = `please insert value a 0 to 9 your have chance ${chance}`;
     error.style.color = "red";
   }
 });
